6e565a4de2cc132097f35345c4dcfc3786c0c578
nithish <nithishpandiyan75@gmail.com>
2025-07-27 22:33:36 +0530

    added cors origin

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    // origin: process.env.CLIENT_URL,
-    origin: "*",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
