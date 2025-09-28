2409ca9a5a3196267067a1cde7c7338641d52565
nithish <nithishpandiyan75@gmail.com>
2025-09-29 00:07:56 +0530

    fixed bug

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,4 +1,8 @@
+const { default: mongoose } = require("mongoose");
+const examSubmissionSchema = require("../models/examSubmissionSchema");
+const reviewModel = require("../models/ReviewModel");
 const userModel = require("../models/userModel");
+const userPassSchema = require("../models/userPassSchema");
 const sendMail = require("../utils/sendMail");
 
 const triggerMail = async (req, res) => {
@@ -25,4 +29,66 @@ const triggerMail = async (req, res) => {
   }
 };
 
-module.exports = { triggerMail };
+const deleteUserEntirely = async (req, res) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    const { userId } = req.params;
+
+    if (!userId) {
+      await session.abortTransaction();
+      return res
+        .status(400)
+        .json({ success: false, message: "User ID is required" });
+    }
+
+    const user = await userModel.findById(userId).session(session);
+    if (!user) {
+      await session.abortTransaction();
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    if (user.role === "admin") {
+      await session.abortTransaction();
+      return res
+        .status(403)
+        .json({ success: false, message: "Admin users cannot be deleted" });
+    }
+
+    if (user.role === "student") {
+      await examSubmissionSchema.deleteMany({ userId }, { session });
+      await userPassSchema.deleteMany({ userId }, { session });
+    }
+
+    if (user.role === "evaluator") {
+      // Remove all reviews added by this evaluator
+      await reviewModel.deleteMany({ evaluator: userId }, { session });
+      await examSubmissionSchema.updateMany(
+        { "reviews.evaluator": userId },
+        { $pull: { reviews: { evaluator: userId } } },
+        { session }
+      );
+    }
+
+    // Finally, delete the user itself
+    await userModel.findByIdAndDelete(userId, { session });
+
+    await session.commitTransaction();
+    res
+      .status(200)
+      .json({ success: true, message: "User deleted successfully" });
+  } catch (error) {
+    await session.abortTransaction();
+    console.error("Delete user transaction error:", error);
+    res
+      .status(500)
+      .json({ success: false, message: "Unable to delete the user" });
+  } finally {
+    session.endSession();
+  }
+};
+
+module.exports = { triggerMail, deleteUserEntirely };
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const { triggerMail } = require("../controllers/adminController");
+const {
+  triggerMail,
+  deleteUserEntirely,
+} = require("../controllers/adminController");
 const router = express.Router();
 
 router.post("/trigger-mail", triggerMail);
+router.delete("/delete-user/:userId", deleteUserEntirely);
 
 module.exports = router;
